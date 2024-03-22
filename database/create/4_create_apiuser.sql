e6870afb86506f93efea98c1aaa5c1578290b277
Michael <84002381+MichaelKane-T@users.noreply.github.com>
2024-03-22 17:28:47 -0600

    apiuser password

--- database/create/4_create_apiuser.sql
+++ database/create/4_create_apiuser.sql
@@ -1,5 +1,5 @@
 CREATE USER 'apiuser'@'localhost' IDENTIFIED BY 'apiuserpass';
 CREATE USER 'apiuser'@'%' IDENTIFIED BY 'apiuserpass';
 GRANT ALL PRIVILEGES ON *.* TO 'apiuser'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'apiuser'@'%', 'apiuser'@'172.29.0.2' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'apiuser'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
