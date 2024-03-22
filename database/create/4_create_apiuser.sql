b280801748bfa8d4f3ef38b01e8be5e1b44d634b
Michael <84002381+MichaelKane-T@users.noreply.github.com>
2024-03-22 17:37:39 -0600

    apiuser twick

--- database/create/4_create_apiuser.sql
+++ database/create/4_create_apiuser.sql
@@ -1,5 +1,3 @@
-CREATE USER 'apiuser'@'localhost' IDENTIFIED BY 'apiuserpass';
 CREATE USER 'apiuser'@'%' IDENTIFIED BY 'apiuserpass';
-GRANT ALL PRIVILEGES ON *.* TO 'apiuser'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'apiuser'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON cs415website.* TO 'apiuser'@'%';
 FLUSH PRIVILEGES;
