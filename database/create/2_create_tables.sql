ef70b17b0c2626f923df0eee508300ce3e0c048d
Michael <84002381+MichaelKane-T@users.noreply.github.com>
2024-03-22 17:01:00 -0600

    tables

--- database/create/2_create_tables.sql
+++ database/create/2_create_tables.sql
@@ -1,16 +1,15 @@
 USE cs415website;
 
-CREATE TABLE User (
-  user_id int NOT NULL AUTO_INCREMENT,
-  first_name varchar(25) NOT NULL,
-  second_name varchar(30) NOT NULL,
-  email varchar(40) NOT NULL,
-  `password` varchar(40) NOT NULL,
-  created_date datetime DEFAULT NULL,
-  is_active tinyint(1) DEFAULT NULL,
-  last_login datetime DEFAULT NULL,
-  PRIMARY KEY (user_id),
-  UNIQUE (email)
+    CREATE TABLE User(
+
+    first_name VARCHAR(35), 
+    second_name VARCHAR(35),
+    pass_word VARCHAR(35), 
+    recovery_key VARCHAR(35), 
+    date_created DATETIME,
+    email VARCHAR(35),
+    user_id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (user_id) 
 );
 
 CREATE TABLE AddressType (
@@ -19,26 +18,62 @@ CREATE TABLE AddressType (
     PRIMARY KEY (address_type_id)
 );
 
+   
 CREATE TABLE UserAddress (
-    user_address_id INT NOT NULL AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    street_1 VARCHAR(50),
-    street_2 VARCHAR(50),
-    city VARCHAR(35),
-    st VARCHAR(2),
-    zip VARCHAR(10),
-    country VARCHAR(30),
-    address_type_id INT NOT NULL,
-    PRIMARY KEY (user_address_id),
-    FOREIGN KEY (user_id) REFERENCES User(user_id),
-    FOREIGN KEY (address_type_id) REFERENCES AddressType(address_type_id)
+        Address_1 VARCHAR(35), 
+        Address_2 VARCHAR(35),
+        City VARCHAR(35), 
+        user_id INT NOT NULL,
+        Zip VARCHAR(35), 
+        Country VARCHAR(35), 
+        last_date_updated DATETIME,
+        email VARCHAR(35),
+        user_address_id INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY (user_address_id), 
+        FOREIGN KEY (user_id) REFERENCES User(user_id)
 );
 
+    
+    CREATE TABLE Team (
+    team_name VARCHAR(35), 
+    team_points INT,
+    log_position INT, 
+    team_form FLOAT, 
+    strength_overall_home FLOAT,
+    strength_overall_away FLOAT,
+    win INT,
+    loss INT,
+    draw INT,
+    team_id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (team_id) 
+);
+
+
+    CREATE TABLE Player (
+    player_id INT NOT NULL AUTO_INCREMENT, 
+    first_name VARCHAR(35),
+    second_name VARCHAR(35),
+    points_per_game INT,
+    total_points INT,
+    goals_scored INT,
+    team_id INT NOT NULL,
+    assists INT,
+    form INT,
+    clean_sheets INT,
+    chance_of_playing_next_round INT,
+    yellow_cards INT,
+    red_cards INT,
+    saves INT,
+    penalties_missed INT,
+    PRIMARY KEY (player_id), 
+    FOREIGN KEY (team_id) REFERENCES Team(team_id)
+);
+ 
 CREATE TABLE UserInfo (
     user_info_id INT NOT NULL AUTO_INCREMENT,
     user_id INT NOT NULL,
     profile_bio VARCHAR(500),
-    profile_picture VARCHAR(150),
+    profile_picture VARCHAR(100),
     modified_date DATETIME,
     created_date DATETIME,
     PRIMARY KEY (user_info_id),
@@ -64,16 +99,10 @@ CREATE TABLE UserPhone (
 );
 
 CREATE TABLE PageData (
-  page_data_id int NOT NULL AUTO_INCREMENT,
-  page_name varchar(150) NOT NULL,
-  page_title varchar(150) NOT NULL,
-  page_description text NOT NULL,
-  page_picture varchar(250) NOT NULL,
-  page_menu varchar(35) NOT NULL,
-  PRIMARY KEY (page_data_id)
+    page_data_id INT NOT NULL AUTO_INCREMENT,
+    page_name VARCHAR(25) NOT NULL,
+    page_title VARCHAR(25) NOT NULL,
+    page_description VARCHAR(150) NOT NULL,
+    page_picture VARCHAR(100),
+    PRIMARY KEY (page_data_id)
 );
-
-
-
-
-
