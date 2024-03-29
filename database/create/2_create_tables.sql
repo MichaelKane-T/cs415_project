a2e551b7384a4de1f618d3f47a3192bc6aa85f30
Michael <84002381+MichaelKane-T@users.noreply.github.com>
2024-03-29 13:59:12 -0600

    database config

--- database/create/2_create_tables.sql
+++ database/create/2_create_tables.sql
@@ -1,16 +1,14 @@
-USE cs415website;
-
-CREATE TABLE User(
-    user_id int NOT NULL AUTO_INCREMENT,
-    first_name varchar(25) NOT NULL,
-    second_name varchar(30) NOT NULL,
-    email varchar(40) NOT NULL,
-    `password` varchar(40) NOT NULL,
-    created_date datetime DEFAULT NULL,
-    is_active tinyint(1) DEFAULT NULL,
-    last_login datetime DEFAULT NULL,
-    PRIMARY KEY (user_id),
-    UNIQUE (email)
+CREATE TABLE User (
+  user_id int NOT NULL AUTO_INCREMENT,
+  first_name varchar(25) NOT NULL,
+  last_name varchar(30) NOT NULL,
+  email varchar(40) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  created_date datetime DEFAULT NULL,
+  is_active tinyint(1) DEFAULT NULL,
+  last_login datetime DEFAULT NULL,
+  PRIMARY KEY (user_id),
+  UNIQUE (email)
 );
 
 CREATE TABLE AddressType (
@@ -27,45 +25,11 @@ CREATE TABLE UserAddress (
     city VARCHAR(35),
     st VARCHAR(2),
     zip VARCHAR(10),
-    country VARCHAR(35),
+    country VARCHAR(30),
     address_type_id INT NOT NULL,
     PRIMARY KEY (user_address_id),
     FOREIGN KEY (user_id) REFERENCES User(user_id),
     FOREIGN KEY (address_type_id) REFERENCES AddressType(address_type_id)
-);
-
-CREATE TABLE Team (
-    team_name VARCHAR(35),
-    team_points INT,
-    log_position INT,
-    team_form FLOAT,
-    strength_overall_home FLOAT,
-    strength_overall_away FLOAT,
-    win INT,
-    loss INT,
-    draw INT,
-    team_id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (team_id)
-);
-
-CREATE TABLE Player (
-    player_id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(35),
-    second_name VARCHAR(35),
-    points_per_game INT,
-    total_points INT,
-    goals_scored INT,
-    team_id INT NOT NULL,
-    assists INT,
-    form INT,
-    clean_sheets INT,
-    chance_of_playing_next_round INT,
-    yellow_cards INT,
-    red_cards INT,
-    saves INT,
-    penalties_missed INT,
-    PRIMARY KEY (player_id),
-    FOREIGN KEY (team_id) REFERENCES Team(team_id)
 );
 
 CREATE TABLE UserInfo (
@@ -98,11 +62,15 @@ CREATE TABLE UserPhone (
 );
 
 CREATE TABLE PageData (
-    page_data_id INT NOT NULL AUTO_INCREMENT,
-    page_name VARCHAR(150) NOT NULL,
-    page_title VARCHAR(150) NOT NULL,
-    page_description TEXT NOT NULL,
-    page_picture VARCHAR(250),
-    page_menu VARCHAR(35),
-    PRIMARY KEY (page_data_id)
+  page_data_id int NOT NULL AUTO_INCREMENT,
+  page_name varchar(150) NOT NULL,
+  page_title varchar(150) NOT NULL,
+  page_description text NOT NULL,
+  page_picture varchar(250) NOT NULL,
+  page_menu varchar(35) NOT NULL,
+  PRIMARY KEY (page_data_id)
 );
+
+
+
+
